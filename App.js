719aff52e8beec3578e64dff96e913068a4ca47c
Dakari Sow <dddddakari@gmail.com>
2025-02-05 12:59:33 -0700

    Added stylings with Tailwind
    Tailwind styling for background and to make sure image is appropriate size.

--- App.js
+++ App.js
@@ -13,3 +13,14 @@ export default function App() {
         source={require('./assets/discordguy.png')}
       />
     </View>
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+  },
+  iconMain: {
+    width: 100,
+    height: 100,
+    marginTop: '40%' ,
