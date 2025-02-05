b52388a48e05eb07951df86a105c418e6f714d9f
Dakari Sow <dddddakari@gmail.com>
2025-02-05 12:58:19 -0700

    Added Logo to App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+// forgot to do just my part not everything created!.
+
+import React from 'react';
+import { View, Image, StyleSheet, Text, TouchableOpacity, Alert} from 'react-native';
+
+// Dakari's Part: The Background and Logo!
+
+export default function App() {
+
+    <View style={styles.container}>
+      <Image
+        style={styles.iconMain}
+        source={require('./assets/discordguy.png')}
+      />
+    </View>
